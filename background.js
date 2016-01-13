57d0a6937483edf958780ed99323afc89958f02d
Emil Alasgarov <debianmik@gmail.com>
2016-01-13 23:47:02 +0400

    Now extension runs in background

--- background.js
+++ background.js
@@ -1,13 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.tabs.onActivated.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "content_script.js"});
-
-  // chrome.extension.onMessage.addListener(
-  // 	function(message, sender, sendResponse){
-  // 		if(message.type == 'getTabId'){
-  // 			sendResponse({tabId:sender.tab.id});
-  // 		}
-  // 	}
-  // );
   chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
     if(message.greeting == false){
       chrome.tabs.query({ active: true },function(tabs){
