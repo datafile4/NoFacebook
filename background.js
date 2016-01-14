e3435595c72d3816986fb42e1675f14c2cd0eb7a
Emil Alasgarov <debianmik@gmail.com>
2016-01-14 11:27:12 +0400

    Added comments

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
 //start when activate tab
 chrome.tabs.onActivated.addListener(function(tab) {
-  //detect facebook tab
+  //RegExp to detect facebook
   var reg = /\bfacebook/g;
+  //chrome query fot detect fb tab
   chrome.tabs.query({'active':true,'lastFocusedWindow':true},function(tabs){
       var url = tabs[0].url;
       var res = url.match(reg);
-      if(res.length !=null){
-        console.log("foo");
+      if(res !=null){
+        //execute content_script.js
         chrome.tabs.executeScript(null, {file: "content_script.js"});
+        //listener for message from content_script.js
         chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+          //if message from content_script.js is false, tab will be closed
           if(message.greeting == false){
             chrome.tabs.query({ active: true },function(tabs){
               chrome.tabs.remove(tabs[0].id);
--- content_script.js
+++ content_script.js
@@ -1,13 +1,9 @@
 setInterval (function(){
+	//Facebook posts placed inside div with "_1dwg" class
 	var kol = document.getElementsByClassName ("_1dwg");
 	var size = kol.length;
 	var tabId;
 	if (size >20) {
-		console.log("Fuck Facebook");
-		// 	chrome.extension.sendMessage({type:getTabId},function(res){
-		// 		tabId = res.tabId;
-		// 	});
-		// alert(tabId);
 		chrome.runtime.sendMessage({greeting: false}, function(response) {
 	  	console.log(response.farewell);
 		});
