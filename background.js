7ce1a7abc40d9fa27dda39101711e9c33ae604dd
Emil Alasgarov <debianmik@gmail.com>
2016-01-14 11:15:34 +0400

    now extension detecs active tab with facebook and closes it after 13 posts on feed

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
+//start when activate tab
 chrome.tabs.onActivated.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
-  chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-    if(message.greeting == false){
-      chrome.tabs.query({ active: true },function(tabs){
-        chrome.tabs.remove(tabs[0].id);
-      });
-    }
+  //detect facebook tab
+  var reg = /\bfacebook/g;
+  chrome.tabs.query({'active':true,'lastFocusedWindow':true},function(tabs){
+      var url = tabs[0].url;
+      var res = url.match(reg);
+      if(res.length !=null){
+        console.log("foo");
+        chrome.tabs.executeScript(null, {file: "content_script.js"});
+        chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+          if(message.greeting == false){
+            chrome.tabs.query({ active: true },function(tabs){
+              chrome.tabs.remove(tabs[0].id);
+            });
+          }
+        });
+      }
   });
+
+
 });
